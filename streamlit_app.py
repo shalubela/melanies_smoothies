f65ae58b57c7f5864470c290a056902c7b95ada3
Shalmalee B <36418884+shalubela@users.noreply.github.com>
2024-05-20 23:29:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ session=cnx.session()
 customer_name=st.text_input('Name on Smoothie: ')
 st.write("Here's the text input: ", customer_name)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col(search_on)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col(search_on))
 ##st.dataframe(data=my_dataframe, use_container_width=True)
 ##st.stop()
 
@@ -39,9 +39,9 @@ if ingredients_list :
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         #st.text(fruityvice_response.json())
-        #if fruityvice_response:
-         #   st.write(":" + fruit_chosen.lower() + ": " + fruit_chosen + ' Nutrition Information:')
-          #  fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        if fruityvice_response:
+            st.write(":" + fruit_chosen.lower() + ": " + fruit_chosen + ' Nutrition Information:')
+            fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(name_on_order, ingredients)
             values ('"""+ customer_name + """', '""" + ingredients_string + """')"""
